c5c6b650e230ae6fd025091ef30ada08e707411b
MBALVIN <miguelob18@hotmail.com>
2024-09-02 01:09:20 -0300

    usuario create, list and cors

--- api/main.py
+++ api/main.py
@@ -1,14 +1,22 @@
-# from flask import Flask, request, jsonify
 import sys
 import sqlite3
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 from pydantic import BaseModel
-import sqlite3
-from typing import List
+from typing import List,Optional
+from fastapi.middleware.cors import CORSMiddleware
 
 version = f"{sys.version_info.major}.{sys.version_info.minor}"
 
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000","https://padel-reservas.onrender.com/"],  # Origins allowed to access the backend
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 
 @app.get("/")
@@ -120,6 +128,110 @@ async def get_reservas():
     
     return reservas_list
 
+# Database model for the Usuario
+class Usuario(BaseModel):
+    usuarioid: int
+    alias: str
+    contrasena: str
+    nombre: str
+    apellido: str
+    genero: str
+    edad: str
+    direccion: str
+    email: str
+    telefono: str
+    nivel: str
+    tipoJuego: str
+    fotoperfil: Optional[str]  # Assuming a base64 string
+    idtipousuario: int
+
+# Conectar a la base de datos y crear la tabla si no existe
+def init_db():
+    conn = sqlite3.connect('usuarios.db')
+    c = conn.cursor()
+    c.execute('''
+              CREATE TABLE IF NOT EXISTS usuarios
+              (usuarioid INTEGER PRIMARY KEY AUTOINCREMENT,
+              alias TEXT,
+              contrasena,
+              nombre TEXT,
+              apellido TEXT,
+              genero TEXT,
+              edad TEXT,
+              direccion TEXT,
+              email TEXT,
+              telefono TEXT,
+              nivel TEXT,
+              tipoJuego TEXT,
+              fotoperfil TEXT,
+              idtipousuario INTEGER)
+              ''')
+    conn.commit()
+    conn.close()
+
+init_db()
+
+@app.post("/usuario")
+async def create_usuario(usuario: Usuario):
+    conn = sqlite3.connect('usuarios.db')
+    c = conn.cursor()
+    c.execute("""
+        INSERT INTO usuarios 
+        (alias, contrasena, nombre, apellido, genero, edad, direccion, email, telefono, nivel, tipoJuego, fotoperfil, idtipousuario) 
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        """,
+        (usuario.alias, usuario.contrasena, usuario.nombre, usuario.apellido, usuario.genero, usuario.edad,
+         usuario.direccion, usuario.email, usuario.telefono, usuario.nivel, usuario.tipoJuego, 
+         usuario.fotoperfil, usuario.idtipousuario)
+    )
+    conn.commit()
+    conn.close()
+    
+    return {"message": "Usuario creado con \u00e9xito"}
+
+# Endpoint to retrieve all usuarios
+@app.get("/usuarios", response_model=List[Usuario])
+async def get_usuarios():
+    conn = sqlite3.connect('usuarios.db')
+    c = conn.cursor()
+    c.execute('''
+        SELECT usuarioid, alias, contrasena, nombre, apellido, genero, edad, direccion, email, telefono, nivel, tipoJuego, fotoperfil, idtipousuario
+        FROM usuarios
+    ''')
+    rows = c.fetchall()
+    conn.close()
+
+    usuarios = [Usuario(
+        usuarioid=row[0], alias=row[1], contrasena=row[2], nombre=row[3], apellido=row[4], genero=row[5], 
+        edad=row[6], direccion=row[7], email=row[8], telefono=row[9], nivel=row[10], 
+        tipoJuego=row[11], fotoperfil=row[12], idtipousuario=row[13]
+    ) for row in rows]
+
+    return usuarios  
+
+# Endpoint to retrieve a usuario by alias and contrasena
+@app.get("/usuario", response_model=Usuario)
+async def get_usuario(alias: str = Query(...), contrasena: str = Query(...)):
+    conn = sqlite3.connect('usuarios.db')
+    c = conn.cursor()
+    c.execute('''
+        SELECT usuarioid, alias, contrasena, nombre, apellido, genero, edad, direccion, email, telefono, nivel, tipoJuego, fotoperfil, idtipousuario
+        FROM usuarios WHERE alias = ? AND contrasena = ?
+    ''', (alias, contrasena))
+    row = c.fetchone()
+    conn.close()
+
+    if row:
+        usuario = Usuario(
+            usuarioid=row[0], alias=row[1], contrasena=row[2], nombre=row[3], apellido=row[4], genero=row[5],
+            edad=row[6], direccion=row[7], email=row[8], telefono=row[9], 
+            nivel=row[10], tipoJuego=row[11], fotoperfil=row[12], idtipousuario=row[13]
+        )
+        return usuario
+    else:
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
+
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8181)
+    
